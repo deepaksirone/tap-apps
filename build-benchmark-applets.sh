aa6d47750cc0ed40552ff176aa0804de51447061
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2023-07-04 17:54:27 -0500

    Move the sed stuff into the build script

--- build-benchmark-applets.sh
+++ build-benchmark-applets.sh
@@ -4,18 +4,18 @@
 set -ex
 
 function usage() {
-	echo "build-benchmark-applets.sh <benchmark_dir> <output_dir>"
+	echo "build-benchmark-applets.sh <benchmark_dir> <output_dir> <triggerHostname> <triggerPort> <actionHostname> <actionPort>"
 	exit 1
 }
 
-if [ -z "$1" ]; then
-	usage
-fi
-
-if [ -z "$2" ]; then
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ]; then
         usage
 fi
 
+triggerHostname=$3
+triggerPort=$4
+actionHostname=$5
+actionPort=$6
 
 if [ -z "$RULE_SHIM_DIR" ]; then
 	echo "RULE_SHIM_DIR not set"
@@ -34,7 +34,13 @@ then
 fi
 
 #export RULE_LIB_DIR=$1/output
+#git checkout .
+
 mkdir -p build
+sed -i "270s/.*/     http::Request request{\"http:\/\/$triggerHostname:$triggerPort\/event_data\/\"};/" src/encrypted_rule/host/enclave-host.cpp
+sed -i "13s/.*/    #define RULE_ACTION_PARAMS_UNESCAPED { \"{ \\\\\"attrib1\\\\\" : \\\\\"val1\\\\\", \\\\\"attrib2\\\\\" : \\\\\"val2\\\\\", \\\\\"action_url\\\\\": \\\\\"http:\/\/$actionHostname:$actionPort\/action_data\/\\\\\", \\\\\"action_id\\\\\": \\\\\"0\\\\\" }\",}/" src/encrypted_rule/eapp/rule_params.h
+sed -i "289s/.*/     http::Request request{\"http:\/\/$triggerHostname:$triggerPort\/event_data\/\"};/" src/rule_process/host/enclave-host.cpp
+sed -i "13s/.*/    #define RULE_ACTION_PARAMS_UNESCAPED { \"{ \\\\\"attrib1\\\\\" : \\\\\"val1\\\\\", \\\\\"attrib2\\\\\" : \\\\\"val2\\\\\", \\\\\"action_url\\\\\": \\\\\"http:\/\/$actionHostname:$actionPort\/action_data\/\\\\\", \\\\\"action_id\\\\\": \\\\\"0\\\\\" }\",}/" src/rule_process/eapp/rule_params.h
 
 for file in $1/*;
 do
